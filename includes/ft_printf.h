e0b17214a4932fc0894bc3583885e31d10675997
kinamura <i.kosuke0801@gmail.com>
2024-05-10 14:11:54 +0900

    args

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -1,8 +1,38 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <unistd.h>
+#include "libft.h"
 #include <stdarg.h>
+
+#define BASE "0123456789ABCDEF"
+
+typedef struct  s_args  
+{
+    va_list ap;
+    char    *fmt;
+    int     index;
+    int     length;
+    int     digit;
+    int     base;
+    int     minus_flag;
+    int     plus_flag;
+    int     space_flag;
+    int     sharp_flag;
+    int     comma_flag;
+    int     asterisk_flag;
+    int     width_flag;
+    int     precision_flag;
+    int     hh_flag;
+    int     h_flag;
+    int     l_flag;
+    int     ll_flag;
+    int     j_flag;
+    int     z_flag;
+    int     t_flag;
+    int     error_flag;
+}   t_args;
+
+int     ft_printf(const char *format, ...);
 
 
 
--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -1,6 +1,45 @@
 #include "ft_printf.h"
 
+void    init(t_args *args)
+{
+    args->digit = 0;
+    args->base = 0;
+    args->minus_flag = 0;
+    args->plus_flag = 0;
+    args->space_flag = 0;
+    args->sharp_flag = 0;
+    args->comma_flag = 0;
+    args->asterisk_flag = 0;
+    args->width_flag = 0;
+    args->precision_flag = 0;
+    args->hh_flag = 0;
+    args->h_flag = 0;
+    args->l_flag = 0;
+    args->ll_flag = 0;
+    args->j_flag = 0;
+    args->z_flag = 0;
+    args->t_flag = 0;
+    args->error_flag = 0;
+}
+
 int ft_printf(const char *format, ...)
 {
-    
+    t_args  *args;
+    int     ret;
+
+    if (!format)
+        return (-1);
+    args = (t_args *)malloc(sizeof(t_args));
+    if (!args)
+        return (-1);
+    ft_init(args);
+    args->length = 0;
+    args->index = 0;
+    args->fmt = format;
+    va_start(args->ap, args);
+    ft_printf_start(args->ap, format);
+    va_end(args->ap);
+    ret = args->length;
+    free(args);
+    return (ret);
 }
