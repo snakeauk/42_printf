b47dc4a29624b84be3e0c0444afcea8527a16540
kinamura <i.kosuke0801@gmail.com>
2024-09-14 00:20:08 +0900

    ft_ulltoa_base

---
+++ srcs/ft_ulltoa_base.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ulltoa_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kinamura <kinamura@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/12 06:47:56 by kinamura          #+#    #+#             */
+/*   Updated: 2024/09/14 00:09:22 by kinamura         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+char *ft_ulltoa_base(unsigned long long unum, char *base)
+{
+    char				*ret;
+	ssize_t				size;
+    unsigned long long  base_len;
+    
+	if (unum == 0)
+		return (ft_strdup("0"));
+    base_len = (unsigned long long)ft_strlen(base);
+    size = ft_ullsize(unum, base);
+    ret = (char *)malloc(sizeof(char) * (size + 1));
+	if (!ret)
+		return (NULL);
+	ret[size] = '\0';
+	while (size > 0)
+	{
+		ret[--size] = base[unum % base_len];
+		unum /= base_len;
+	}
+    return (ret);
+}
