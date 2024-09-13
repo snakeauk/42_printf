f00918aaf47d61c61daae0e878219ca56bbd82a7
kinamura <i.kosuke0801@gmail.com>
2024-09-14 00:20:23 +0900

    ft_vprintf

---
+++ srcs/ft_vprintf.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_vprintf.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kinamura <kinamura@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/12 06:16:22 by kinamura          #+#    #+#             */
+/*   Updated: 2024/09/13 16:13:38 by kinamura         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int ft_printf_switch(const char *format, va_list *ap, int fd)
+{
+    if (*format == '%')
+        return (ft_putc('%', fd));
+    else if (*format == 'c')
+        return (ft_putc(va_arg(*ap, int), fd));
+    else if (*format == 's')
+        return (ft_fputs(va_arg(*ap, char *), fd));
+    else if (*format == 'u')
+        return (ft_fputu(va_arg(*ap, unsigned int), fd));
+	else if (*format == 'd' || *format == 'i')
+		return (ft_fputi(va_arg(*ap, int), fd));
+	else if (*format == 'x' || *format == 'X')
+		return (ft_fputx(va_arg(*ap, unsigned int), fd, *format));
+	else if (*format == 'p')
+		return (ft_fputp(va_arg(*ap, unsigned long long), fd));
+	else
+		return (-1);
+}
+
+int ft_vprintf(const char *format, va_list ap)
+{
+	int ret;
+	int w_ret;
+
+	ret = 0;
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            if ((w_ret = ft_printf_switch(format, &ap, STDOUT_FILENO)) < 0)
+                return (-1);
+            ret += w_ret;
+        }
+        else
+        {
+            if ((w_ret = ft_putc(*format, STDOUT_FILENO)) < 0)
+                return (-1);
+            ret += w_ret;
+        }
+        format++;
+    }
+    return (ret);
+}
