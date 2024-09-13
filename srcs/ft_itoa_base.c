7d3d571470ec422a5d28adfc5b4ed116cc5d58d5
kinamura <i.kosuke0801@gmail.com>
2024-09-14 00:18:31 +0900

    ft_itoa_base

---
+++ srcs/ft_itoa_base.c
@@ -0,0 +1,31 @@
+
+#include "ft_printf.h"
+
+char	*ft_itoa_base(int n, char *base)
+{
+	long long	ln;
+	char		*ret;
+	size_t		size;
+	size_t		base_len;
+
+	if (n == 0)
+		return (ft_strdup("0"));
+	ln = n;
+	size = ft_isize(n, base);
+	base_len = ft_strlen(base);
+	if ((ret = (char *)malloc(sizeof(char) * (size + 1))) == NULL)
+		return (NULL);
+	ret[size--] = '\0';
+	if (ln < 0)
+	{
+		ln = -ln;
+		ret[0] = '-';
+	}
+	while (ln >= (long long)base_len)
+	{
+		ret[size--] = base[ln % base_len];
+		ln /= base_len;
+	}
+	ret[size] = base[ln % base_len];
+	return (ret);
+}
