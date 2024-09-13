5314acb0101527f211f9a8c913efb09161b39e98
kinamura <i.kosuke0801@gmail.com>
2024-09-14 00:19:51 +0900

    ft_ullsize

---
+++ srcs/ft_ullsize.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ullsize.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kinamura <kinamura@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/12 06:48:39 by kinamura          #+#    #+#             */
+/*   Updated: 2024/09/13 23:57:11 by kinamura         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+size_t ft_ullsize(unsigned long long unum, char *base)
+{
+	int		ret;
+    size_t	base_len;
+
+    base_len = ft_strlen(base);
+	ret = 0;
+	if (unum == 0)
+		return (1);
+	while (unum != 0)
+	{
+		unum /= (unsigned long long)base_len;
+		ret++;
+	}
+	return (ret);
+}
